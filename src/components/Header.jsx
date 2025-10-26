84310ebc20ccd40c79b8c214d01476ff1ed9b967
riyashelarrr <riyashelar137@gmail.com>
2025-10-26 09:05:13 -0700

    added logo on top left

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Box, Flex, Heading, Spacer } from "@chakra-ui/react";
+import { Box, Flex, Heading, Spacer, Image} from "@chakra-ui/react";
 import { ColorModeButton } from "./ui/color-mode";
+import logo from "../assets/logo.png";
 
 export const Header = () => {
   return (
     <Box as="header" width="100%" p={4} borderBottomWidth="1px">
       <Flex>
+        <Image src={logo} alt="Logo" boxSize="50px" mr={3} />
         <Heading size="md">Visionary Code Project</Heading>
         <Spacer />
         <ColorModeButton />
